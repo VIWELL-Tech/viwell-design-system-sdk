36c0f49b1b6e985e68a701dd3438a6f480e2e4d1
farag-ahmed <162093146+farag-ahmed@users.noreply.github.com>
2024-04-23 21:15:25 +0400

    build-main-1713892525

--- dist/components/index.d.ts
+++ dist/components/index.d.ts
@@ -1,3 +1,4 @@
 export * from './basic';
+export * from './icons';
 export * from './molecules';
 export * from './organisms';
--- dist/web-design-system.es.js
+++ dist/web-design-system.es.js
@@ -1,17 +1,18 @@
-import { jsx as o } from "react/jsx-runtime";
-const c = {
+import { jsx as s } from "react/jsx-runtime";
+import { forwardRef as i, memo as g } from "react";
+const T = {
   PRIMARY: "primary",
   SECONDARY: "secondary",
   TERTIARY: "tertiary"
-}, i = {
+}, d = {
   PRIMARY: "bg-green-200 hover:bg-green-400 active:bg-green-500",
   SECONDARY: "bg-blue-200 hover:bg-blue-400 active:bg-blue-500",
   TERTIARY: "bg-red-200 hover:bg-red-400 active:bg-red-500"
-}, E = ({ label: t, variant: n = "PRIMARY", isDisabled: r = !1, onClick: l }) => /* @__PURE__ */ o(
+}, h = ({ label: t, variant: n = "PRIMARY", isDisabled: o = !1, onClick: l }) => /* @__PURE__ */ s(
   "button",
   {
-    className: `text-5xl transition-colors px-6 py-2 rounded-md ${i[n]} ${r ? "bg-gray-300 text-slate-600 cursor-not-allowed pointer-events-none" : ""}`,
-    onClick: r ? l : void 0,
+    className: `text-5xl transition-colors px-6 py-2 rounded-md ${d[n]} ${o ? "bg-gray-300 text-slate-600 cursor-not-allowed pointer-events-none" : ""}`,
+    onClick: o ? l : void 0,
     children: t
   }
 ), e = {
@@ -28,13 +29,13 @@ const c = {
   SMALL_12_M: "p-sm-12-m",
   SMALL_10: "p-sm-10",
   ALL_CAPS_12: "p-all-caps-12"
-}, s = {
+}, r = {
   SYSTEM_BLACK: "system-black",
   SYSTEM_WHITE: "system-white",
   SYSTEM_LIGHT_GREEN: "system-light-green",
   SYSTEM_GREEN: "system-green",
   SYSTEM_RED: "system-red"
-}, g = (t) => t.startsWith("h4") ? "h4" : t.startsWith("p") ? "p" : t, x = {
+}, x = (t) => t.startsWith("h4") ? "h4" : t.startsWith("p") ? "p" : t, c = {
   [e.H1]: "text-3xl font-medium leading-4xl",
   [e.H2]: "text-2xl font-medium leading-3xl",
   [e.H3]: "text-xl font-medium leading-2xl",
@@ -48,25 +49,32 @@ const c = {
   [e.SMALL_12_M]: "text-xs font-medium leading-sm",
   [e.SMALL_10]: "text-xs font-normal leading-xs",
   [e.ALL_CAPS_12]: "text-xs font-normal leading-sm"
-}, d = {
-  [s.SYSTEM_LIGHT_GREEN]: "text-system-light-green",
-  [s.SYSTEM_GREEN]: "text-system-green",
-  [s.SYSTEM_RED]: "text-system-red",
-  [s.SYSTEM_BLACK]: "text-system-black",
-  [s.SYSTEM_WHITE]: "text-system-white"
-}, S = ({ variant: t, color: n = "system-black", text: r, ...l }) => {
-  const m = g(t), a = x[t] + " " + d[n];
-  return /* @__PURE__ */ o(m, { className: a, ...l, children: r });
-}, M = () => /* @__PURE__ */ o("div", { children: "Banner" }), T = () => /* @__PURE__ */ o("div", { children: "Footer" }), R = (t, n = 16) => 1 / n * (t || 0) + "rem";
+}, _ = {
+  [r.SYSTEM_LIGHT_GREEN]: "text-system-light-green",
+  [r.SYSTEM_GREEN]: "text-system-green",
+  [r.SYSTEM_RED]: "text-system-red",
+  [r.SYSTEM_BLACK]: "text-system-black",
+  [r.SYSTEM_WHITE]: "text-system-white"
+}, f = ({ variant: t, color: n = "system-black", text: o, ...l }) => {
+  const m = x(t), a = c[t] + " " + _[n];
+  return /* @__PURE__ */ s(m, { className: a, ...l, children: o });
+}, E = (t, n) => /* @__PURE__ */ s("svg", { xmlns: "http://www.w3.org/2000/svg", width: 11, height: 13, fill: "none", ref: n, ...t, children: /* @__PURE__ */ s(
+  "path",
+  {
+    fill: "#fff",
+    d: "M8.64 6.814c.012-.91.5-1.772 1.277-2.25a2.778 2.778 0 0 0-2.162-1.168c-.91-.096-1.792.544-2.255.544-.473 0-1.186-.535-1.955-.519A2.903 2.903 0 0 0 1.122 4.9c-1.047 1.814-.266 4.48.738 5.945.502.718 1.089 1.52 1.856 1.492.752-.032 1.032-.48 1.94-.48.898 0 1.161.48 1.944.462.807-.014 1.315-.722 1.799-1.446.36-.512.638-1.077.823-1.675A2.613 2.613 0 0 1 8.64 6.814ZM7.16 2.432A2.64 2.64 0 0 0 7.765.54a2.685 2.685 0 0 0-1.737.899 2.534 2.534 0 0 0-.62 1.82 2.246 2.246 0 0 0 1.754-.828Z"
+  }
+) }), M = i(E), A = g(M), Y = () => /* @__PURE__ */ s("div", { children: "Banner" }), p = () => /* @__PURE__ */ s("div", { children: "Footer" }), u = (t, n = 16) => 1 / n * (t || 0) + "rem";
 export {
-  c as AT_BUTTON_VARIANT,
-  E as Button,
-  M as MlBanner,
-  T as OrFooter,
-  S as Text,
-  s as TextColors,
+  T as AT_BUTTON_VARIANT,
+  h as Button,
+  A as Memo,
+  Y as MlBanner,
+  p as OrFooter,
+  f as Text,
+  r as TextColors,
   e as TextVariants,
-  g as generateHTMLTag,
-  R as pxToRem,
-  i as variantClasses
+  x as generateHTMLTag,
+  u as pxToRem,
+  d as variantClasses
 };
